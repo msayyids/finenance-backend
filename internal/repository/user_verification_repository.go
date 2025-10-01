dd86e90bedd35dd03e16f83cc2d7b7e7c2f5bba5
msayyids <muhammadsayyid525@gmail.com>
2025-10-02 00:02:20 +0700

    [feature/verivication] implement query verification user after register

--- internal/repository/user_verification_repository.go
+++ internal/repository/user_verification_repository.go
@@ -6,11 +6,37 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func (vr *UserVerificationRepository) CreateVerificationKey(db *sqlx.Tx) (*entity.UserVerification, error) {
-	query := `INSERT INTO verification_keys (user_id,key,expired_at,created_at)  VALUES (:user_id,:key,:expired_at,)`
+func (vr *UserVerificationRepository) CreateVerificationKey(db *sqlx.Tx, userVerification *entity.UserVerification) (*entity.UserVerification, error) {
+	query := `INSERT INTO verification_keys (user_id, key, expired_at) 
+              VALUES (:user_id, :key, :expired_at) 
+              RETURNING id`
 
-	if err:=db.
+	err := db.QueryRowx(
+		query,
+		userVerification.UserId,
+		userVerification.Key,
+		userVerification.ExpiredAt,
+	).Scan(&userVerification.Id)
 
+	if err != nil {
+		return nil, err
+	}
 
-	return nil
+	return userVerification, nil
+}
+
+func (vr *UserVerificationRepository) FindUserVerifByKey(db *sqlx.Tx, userVerification *entity.UserVerification) (*entity.UserVerification, error) {
+	query := `
+        SELECT id, user_id, key, expired_at
+        FROM verification_keys
+        WHERE user_id = $1 AND key = $2 AND expired_at > NOW()
+        LIMIT 1
+    `
+
+	err := db.Get(userVerification, query, userVerification.UserId, userVerification.Key)
+	if err != nil {
+		return nil, err
+	}
+
+	return userVerification, nil
 }
--- internal/repository/user_verification_repository_implementation.go
+++ internal/repository/user_verification_repository_implementation.go
@@ -1,6 +1,19 @@
 package repository
 
+import (
+	"finenance-app/internal/entity"
+
+	"github.com/jmoiron/sqlx"
+)
+
 type UserVerificationRepository struct {
 }
 
-type UserVerificationRepositoryImplementation interface{}
+type UserVerificationRepositoryImplementation interface {
+	CreateVerificationKey(db *sqlx.Tx, userVerification *entity.UserVerification) (*entity.UserVerification, error)
+	FindUserVerifByKey(db *sqlx.Tx, userVerification *entity.UserVerification) (*entity.UserVerification, error)
+}
+
+func NewUserVerification() UserVerificationRepositoryImplementation {
+	return &UserVerificationRepository{}
+}
